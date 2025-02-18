549091513f619bd38363bc2702703d420be5a97e
i.belkin <>
2025-02-18 13:50:57 +0300

    test

--- demo/demo/Sources/BaseViewController/CartViewCell.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-class CartViewCell: UITableViewCell {
-    
-    @IBOutlet weak var picture: UIImageView!
-    @IBOutlet weak var price: UILabel!    
-    @IBOutlet weak var name: UILabel!
-}
